424fd3e57bb00b8b19c7d4fa509c87f2fc5496fc
yadik64 <45825824+yadik64@users.noreply.github.com>
2024-05-29 11:32:43 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,30 +5,36 @@ import PackageDescription
 
 let package = Package(
     name: "CloudPayments",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CloudPaymentsSDK",
-            targets: ["sdk"]),
-        .library(
-            name: "CloudPaymentsNetworking",
-            targets: ["networking"]),
+            name: "CloudPayments",
+            targets: ["CloudPayments"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.3.0"),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "sdk",
-            path: "sdk",
-            resources: [
-                            .process("../Resources")
-                        ]),
-        .target(
-            name: "networking",
-            path: "networking"),
+            name: "CloudPayments",
+            dependencies: [
+                "Alamofire",
+                "ObjectMapper"
+            ]),
+        .testTarget(
+            name: "CloudPaymentsTests",
+            dependencies: [
+                "Alamofire",
+                "ObjectMapper",
+                "CloudPayments"
+            ]),
     ]
 )
